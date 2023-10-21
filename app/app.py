14aae8ac9d384024f5f8a57e4f16d527dda926e6
gtaiyou24 <gtaiyou24@gmail.com>
2023-10-21 18:49:13 +0900

    feat: fashion-clip image processor

--- app/app.py
+++ app/app.py
@@ -2,8 +2,9 @@ import os
 from contextlib import asynccontextmanager
 
 import torch
-from diffusers import DiffusionPipeline
+from diffusers import PaintByExamplePipeline
 from fastapi import FastAPI
+from transformers import CLIPImageProcessor
 
 from port.adapter.resource.inference import inference_resource
 from port.adapter.resource.ping import ping_resource
@@ -11,10 +12,12 @@ from port.adapter.resource.ping import ping_resource
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    pipe = DiffusionPipeline.from_pretrained(
+    pipe: PaintByExamplePipeline = PaintByExamplePipeline.from_pretrained(
         "Fantasy-Studio/Paint-by-Example",
         torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32
     )
+    processor = CLIPImageProcessor.from_pretrained('patrickjohncyh/fashion-clip')
+    pipe.feature_extractor = processor
     pipe = pipe.to("cuda" if torch.cuda.is_available() else "cpu")
     app.pipe = pipe
     yield
